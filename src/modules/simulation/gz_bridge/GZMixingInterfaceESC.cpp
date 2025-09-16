f4a7f314a2a7729f4f5bd5a0383df16b99acc088
E-Krantz <eliaskra@kth.se>
2025-09-16 13:16:50 +0200

    Intermediate atmos_3d sitl update

--- src/modules/simulation/gz_bridge/GZMixingInterfaceESC.cpp
+++ src/modules/simulation/gz_bridge/GZMixingInterfaceESC.cpp
@@ -33,6 +33,8 @@
 
 #include "GZMixingInterfaceESC.hpp"
 
+#include <uORB/topics/actuator_motors.h>
+
 bool GZMixingInterfaceESC::init(const std::string &model_name)
 {
 
@@ -65,27 +67,75 @@ bool GZMixingInterfaceESC::init(const std::string &model_name)
 bool GZMixingInterfaceESC::updateOutputs(uint16_t outputs[MAX_ACTUATORS], unsigned num_outputs,
 		unsigned num_control_groups_updated)
 {
+	static bool debug_shown = false;
+	if (!debug_shown) {
+		PX4_INFO("GZ ESC updateOutputs called: num_outputs=%u", num_outputs);
+		debug_shown = true;
+	}
+
 	unsigned active_output_count = 0;
 
+	// Find the highest active output index + 1 (don't break early on gaps)
 	for (unsigned i = 0; i < num_outputs; i++) {
 		if (_mixing_output.isFunctionSet(i)) {
-			active_output_count++;
-
-		} else {
-			break;
+			active_output_count = i + 1;
 		}
+	}
+
+	// Debug output
+	static bool debug_once = true;
+	if (debug_once && active_output_count > 0) {
+		PX4_INFO("GZ ESC: num_outputs=%u, active_output_count=%u", num_outputs, active_output_count);
+		for (unsigned i = 0; i < num_outputs; i++) {
+			if (_mixing_output.isFunctionSet(i)) {
+				PX4_INFO("GZ ESC: Output %u is active", i);
+			}
+		}
+		debug_once = false;
 	}
 
 	if (active_output_count > 0) {
 		gz::msgs::Actuators rotor_velocity_message;
 		rotor_velocity_message.mutable_velocity()->Resize(active_output_count, 0);
 
+		// Publish ESC status to mark active ESCs as online
+		esc_status_s esc_status{};
+		esc_status.timestamp = hrt_absolute_time();
+		esc_status.esc_count = math::min((int)active_output_count, (int)esc_status_s::CONNECTED_ESC_MAX);
+
 		for (unsigned i = 0; i < active_output_count; i++) {
 			rotor_velocity_message.set_velocity(i, outputs[i]);
+			
+			// Mark ESC as online if it's active
+			if (i < esc_status.esc_count && _mixing_output.isFunctionSet(i)) {
+				esc_status.esc[i].timestamp = hrt_absolute_time();
+				esc_status.esc[i].esc_rpm = outputs[i]; // Use command as fake RPM
+				esc_status.esc[i].actuator_function = actuator_motors_s::ACTUATOR_FUNCTION_MOTOR1 + i;
+				esc_status.esc_online_flags |= 1 << i;
+				
+				if (outputs[i] > 0) {
+					esc_status.esc_armed_flags |= 1 << i;
+				}
+			}
+		}
+
+		// Publish ESC status
+		if (esc_status.esc_count > 0) {
+			static bool debug_esc_once = true;
+			if (debug_esc_once) {
+				PX4_INFO("GZ ESC: Publishing ESC status for %d ESCs, online_flags=0x%x", 
+					esc_status.esc_count, esc_status.esc_online_flags);
+				debug_esc_once = false;
+			}
+			_esc_status_pub.publish(esc_status);
 		}
 
 		if (_actuators_pub.Valid()) {
 			return _actuators_pub.Publish(rotor_velocity_message);
+		}
+	} else {
+		if (!debug_shown) {
+			PX4_INFO("GZ ESC: No active outputs detected");
 		}
 	}
 
@@ -109,11 +159,12 @@ void GZMixingInterfaceESC::motorSpeedCallback(const gz::msgs::Actuators &actuato
 	pthread_mutex_lock(&_node_mutex);
 
 	esc_status_s esc_status{};
-	esc_status.esc_count = actuators.velocity_size();
+	esc_status.esc_count = math::min((int)actuators.velocity_size(), (int)esc_status_s::CONNECTED_ESC_MAX);
 
-	for (int i = 0; i < actuators.velocity_size(); i++) {
+	for (int i = 0; i < esc_status.esc_count; i++) {
 		esc_status.esc[i].timestamp = hrt_absolute_time();
 		esc_status.esc[i].esc_rpm = actuators.velocity(i);
+		esc_status.esc[i].actuator_function = actuator_motors_s::ACTUATOR_FUNCTION_MOTOR1 + i;
 		esc_status.esc_online_flags |= 1 << i;
 
 		if (actuators.velocity(i) > 0) {
